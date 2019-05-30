0b0366b606d77586d682660f442aa9c5cac0e47f
Mark Agimba <markaguero16@gmail.com>
2019-05-30 11:13:19 +0300

    add url patterns

--- hoodapp/urls.py
+++ hoodapp/urls.py
@@ -14,8 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from django.contrib.auth import views 
+from rest_framework.authtoken.views import obtain_auth_token
+from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',include('hood.urls')),
+    path('accounts/', include('django_registration.backends.one_step.urls')),
+    # path('logout/', views.logout, {"next_page": '/'}),
+    path('api-token-auth/', obtain_auth_token),
+    url(r'^accounts/', include('django.contrib.auth.urls')),
 ]
